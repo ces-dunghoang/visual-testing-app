07247e3bc4dc777c710a691d7441ca2e71acf405
kaito <dung.hoang@codeenginestudio.com>
2023-12-08 17:28:28 +0700

    :feat: communicate with node server by rabbitmq

--- src/main/java/com/example/demoappvisualtesting/controller/MessageController.java
+++ src/main/java/com/example/demoappvisualtesting/controller/MessageController.java
@@ -3,23 +3,23 @@ import com.example.demoappvisualtesting.publisher.RabbitMQProducer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/visual-testing")
-public class VisualTestingController {
+@RequestMapping("/api/v1")
+public class MessageController {
 
   private RabbitMQProducer producer;
 
-  public VisualTestingController(RabbitMQProducer producer) {
+  public MessageController(RabbitMQProducer producer) {
     this.producer = producer;
   }
 
   // http://localhost:8080/api/v1/publish?message=hello
-  @GetMapping
-  public ResponseEntity<String> visualTesting(){
-    String message = "visual-testing";
+  @GetMapping("/publish")
+  public ResponseEntity<String> sendMessage(@RequestParam("message") String message){
     producer.sendMessage(message);
-    return ResponseEntity.ok("We will notify you when completed");
+    return ResponseEntity.ok("Message sent to RabbitMQ ...");
   }
 }
